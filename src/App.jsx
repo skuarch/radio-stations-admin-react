1e0e45a97e040f6165674402a8aaa54c1e61d8f5
skuarch <skuarch@yahoo.com.mx>
2022-12-12 22:29:48 -0600

    Add login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Login from './components/login';
 
 function App() {
-  return <div>Skuarch</div>;
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
 export default App;
